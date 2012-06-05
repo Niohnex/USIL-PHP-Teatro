723f0d49d1e8f0a336276abd6cbd494cadac210e
unknown <JOSE@JOSE-PC.(none)>
2012-06-05 00:40:23 -0500

    Modulo usuarios

---
+++ ModuloUsuarios/scriptDB.sql
@@ -0,0 +1,47 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 04-06-2012 a las 22:40:35
+-- Versión del servidor: 5.5.8
+-- Versión de PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `teatro`
+--
+CREATE DATABASE `teatro` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `teatro`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` int(11) NOT NULL,
+  `nombres` varchar(1000) NOT NULL,
+  `apellidos` varchar(1000) NOT NULL,
+  `correo` varchar(1000) NOT NULL,
+  `sexo` int(11) NOT NULL,
+  `contrasena` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+
+--
+-- Volcar la base de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `tipo`, `nombres`, `apellidos`, `correo`, `sexo`, `contrasena`) VALUES
+(28, 1, 'administrador', '', 'admin', 1, '12345'),
+(29, 0, 'Juan', 'Perez', 'jperes@gmail.com', 1, '123');

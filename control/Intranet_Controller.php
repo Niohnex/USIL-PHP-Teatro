3c13b6cd5cb80e4f9303cbe1b1bf8cb21b51b401
Niohnex <niohnex@joel-RV411-RV511-E3511-S3511-RV711.(none)>
2012-07-01 23:20:45 -0500

    fix and build intranet

--- control/Intranet_Controller.php
+++ control/Intranet_Controller.php
@@ -3,18 +3,34 @@
 	require_once (../model/Obra.php);
 	require_once(../mode/Usuario);
 	
+	if (!empty($_GET['action']))
+	{
+		// Crear objeto e ejecutar accion
+	} else {
+		// Imprimir Pagina por defecto
+	}
 
 	class Intranet_Controller {
-		
+
+		// public function _createObra($v_titulo, $v_resena, $v_autor, $v_fechadesde, $v_fechahasta, $v_puntaje, $v_detallesExtra, $v_numeroSitios, $v_precio, $v_duracion, $v_director, $v_afiche)
 		function add_obra () {
-		
+			$_titulo = $_POST['titulo'];
+			$_resena = $_POST['resena'];
+			$_autor = $_POST['autor'];
+			$_fechadesde = $_POST['fechadesde'];
+			$_fechahasta = $_POST['fechahasta'];
+			$_puntaje = $_POST['puntaje'];
+			$_detallesextra = $_POST['detallesextra'];
+			$_numerositios = $_POST['numerositios'];
+			
 		}
 		
 		function delete_obra () {
+			
 		}
 		
 		function modify_obra () {
-		
+			
 		}
 		
 		
